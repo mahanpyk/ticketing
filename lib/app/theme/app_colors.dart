10cec21412cb14d4d5538feb372c244e1b128a38
Mahan <mahan.p18@gmail.com>
2022-03-05 01:19:34 +0330

    - add app_colors.dart to project for define const color

---
+++ lib/app/theme/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const background = Color(0xFF2B2630);
+  static const surfaceDark = Color(0xFF363843);
+  static const buttonFirstColor = Color(0xFF5E4E6B);
+  static const buttonSecondColor = Color(0xFF5110BB);
+}
